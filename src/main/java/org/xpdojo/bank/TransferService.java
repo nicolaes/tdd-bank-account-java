41984951f3844546a01d4ee071c5cdb5308a29da
NicolaeS <nicolae.stihhi@gmail.com>
2022-01-28 15:46:39 +0200

    Create transfer function

--- src/main/java/org/xpdojo/bank/TransferService.java
+++ src/main/java/org/xpdojo/bank/TransferService.java
@@ -2,6 +2,7 @@ package org.xpdojo.bank;
 
 public class TransferService {
     public static void transfer(Account from, Account to, int i) {
-
+        from.withdraw(i);
+        to.deposit(i);
     }
 }
