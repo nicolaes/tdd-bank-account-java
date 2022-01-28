e49c14ffe5444c630c81cccfc24be2d3c62457d1
NicolaeS <nicolae.stihhi@gmail.com>
2022-01-28 14:49:17 +0200

    First test

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void initialAccount_shouldHaveZeroBalance() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
     }
 }
