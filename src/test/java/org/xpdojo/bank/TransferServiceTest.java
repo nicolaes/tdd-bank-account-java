38cc25ec442f441a866043275449886941deb22d
NicolaeS <nicolae.stihhi@gmail.com>
2022-01-28 15:53:27 +0200

    Test excessive transfer

--- src/test/java/org/xpdojo/bank/TransferServiceTest.java
+++ src/test/java/org/xpdojo/bank/TransferServiceTest.java
@@ -1,18 +1,30 @@
 package org.xpdojo.bank;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.xpdojo.bank.Account.emptyAccount;
 
 public class TransferServiceTest {
+    private Account from;
+    private Account to;
+
+    @BeforeEach
+    public void initializeTransferAccounts() {
+        from = emptyAccount();
+        to = emptyAccount();
+    }
+
     @Test
     public void transferAmount_shouldMoveAmountBetweenTwoAccounts() {
-        Account from = emptyAccount();
         from.deposit(10);
-        Account to = emptyAccount();
         TransferService.transfer(from, to, 1);
         Assertions.assertThat(from.balance()).isEqualTo(9);
         Assertions.assertThat(to.balance()).isEqualTo(1);
+    }
+
+    public void transferAmount_shouldNotWorkIfSourceBalanceTooLow() {
+        Assertions.assertThatThrownBy(() -> TransferService.transfer(from, to, 10));
     }
 }
