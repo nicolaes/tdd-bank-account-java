a4fc5297aca20803fe262d6e83396aa1bd8d1704
NicolaeS <nicolae.stihhi@gmail.com>
2022-01-28 15:03:31 +0200

    Test imports static method

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,17 +4,18 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.xpdojo.bank.Account.emptyAccount;
 
 public class AccountTest {
 
     @Test
     public void initialAccount_shouldHaveZeroBalance() {
-        assertThat(Account.emptyAccount().balance()).isEqualTo(0);
+        assertThat(emptyAccount().balance()).isEqualTo(0);
     }
 
     @Test
     public void deposit() {
-        Account account = Account.emptyAccount();
+        Account account = emptyAccount();
         account.deposit(10);
         assertThat(account.balance()).isEqualTo(10);
     }
